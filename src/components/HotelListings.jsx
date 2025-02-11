a303358340038352b2fac917f0f0a979ef8dff0e
ManupaDev <manupadevsuite@gmail.com>
2025-02-11 16:58:25 +0530

    feat: Implement data fetching and error handling in HotelListings component

--- src/components/HotelListings.jsx
+++ src/components/HotelListings.jsx
@@ -1,11 +1,13 @@
-import { useState } from "react";
+import { getHotels } from "@/lib/api/hotels";
+import { useEffect, useState } from "react";
 import HotelCard from "./HotelCard";
 import LocationTab from "./LocationTab";
-import { getHotels } from "@/lib/api/hotels";
-import { Button } from "@/components/ui/button";
 
 export default function HotelListings() {
   const [hotels, setHotels] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+  const [error, setError] = useState("");
 
   const locations = ["ALL", "France", "Italy", "Australia", "Japan"];
 
@@ -24,29 +26,98 @@ export default function HotelListings() {
             .includes(selectedLocation.toLowerCase());
         });
 
+  useEffect(() => {
+    getHotels()
+      .then((data) => {
+        setHotels(data);
+      })
+      .catch((error) => {
+        setIsError(true);
+        setError(error.message);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section className="px-8 py-8 lg:py-16">
+        <div className="mb-12">
+          <h2 className="text-3xl md:text-4xl font-bold mb-4">
+            Top trending hotels worldwide
+          </h2>
+          <p className="text-lg text-muted-foreground">
+            Discover the most trending hotels worldwide for an unforgettable
+            experience.
+          </p>
+        </div>
+        <div className="flex items-center gap-x-4">
+          {locations.map((location, i) => {
+            return (
+              <LocationTab
+                key={i}
+                selectedLocation={selectedLocation}
+                name={location}
+                onClick={handleSelectedLocation}
+              />
+            );
+          })}
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 mt-4">
+          <p>Loading...</p>
+        </div>
+      </section>
+    );
+  }
+
+  if (isError) {
+    return (
+      <section className="px-8 py-8 lg:py-16">
+        <div className="mb-12">
+          <h2 className="text-3xl md:text-4xl font-bold mb-4">
+            Top trending hotels worldwide
+          </h2>
+          <p className="text-lg text-muted-foreground">
+            Discover the most trending hotels worldwide for an unforgettable
+            experience.
+          </p>
+        </div>
+        <div className="flex items-center gap-x-4">
+          {locations.map((location, i) => {
+            return (
+              <LocationTab
+                key={i}
+                selectedLocation={selectedLocation}
+                name={location}
+                onClick={handleSelectedLocation}
+              />
+            );
+          })}
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 mt-4">
+          <p className="text-red-500">{error}</p>
+        </div>
+      </section>
+    );
+  }
+
   return (
     <section className="px-8 py-8 lg:py-16">
       <div className="mb-12">
         <h2 className="text-3xl md:text-4xl font-bold mb-4">
           Top trending hotels worldwide
         </h2>
-        <Button
-          onClick={async () => {
-            const hotels = await getHotels();
-            setHotels(hotels);
-          }}
-        >
-          Fetch Data
-        </Button>
         <p className="text-lg text-muted-foreground">
           Discover the most trending hotels worldwide for an unforgettable
           experience.
         </p>
       </div>
       <div className="flex items-center gap-x-4">
-        {locations.map((location) => {
+        {locations.map((location, i) => {
           return (
             <LocationTab
+              key={i}
               selectedLocation={selectedLocation}
               name={location}
               onClick={handleSelectedLocation}
--- src/lib/api/hotels.js
+++ src/lib/api/hotels.js
@@ -1,11 +1,10 @@
 export const getHotels = async () => {
-  try {
-    const res = await fetch("http://localhost:8000/api/hotels", {
-      method: "GET",
-    });
-    const data = await res.json();
-    return data;
-  } catch (error) {
-    console.log(error);
+  const res = await fetch("http://localhost:8000/api/hotels", {
+    method: "GET",
+  });
+  if (!res.ok) {
+    throw new Error("Failed to fetch hotels");
   }
+  const data = await res.json();
+  return data;
 };
