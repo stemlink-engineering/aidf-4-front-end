0cf1cbfb93c0e2a018e6a4999d19770ac8f8f73a
ManupaDev <manupadevsuite@gmail.com>
2025-03-21 15:35:51 +0530

    fix: Update backend URL for API requests

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const BACKEND_URL = "http://localhost:8000";
+const BACKEND_URL = "http://aidf-horizone-frontend-manupa.netlify.app/";
 
 export const api = createApi({
   reducerPath: "api",
