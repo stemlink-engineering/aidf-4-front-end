0e90eb2c833c5445d1d0bbb91a4863657d6cff3c
ManupaDev <manupadevsuite@gmail.com>
2025-03-28 23:19:29 +0530

    feat: Enhance CheckoutForm to include token-based authorization for payment session requests

--- src/components/CheckoutForm.jsx
+++ src/components/CheckoutForm.jsx
@@ -6,7 +6,7 @@ import {
   EmbeddedCheckoutProvider,
 } from "@stripe/react-stripe-js";
 import { useCallback } from "react";
-
+import { useAuth } from "@clerk/clerk-react";
 const STRIPE_PUBLISHABLE_KEY = import.meta.env.VITE_STRIPE_PUBLISHABLE_KEY;
 const stripePromise = loadStripe(STRIPE_PUBLISHABLE_KEY);
 
@@ -14,18 +14,24 @@ const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
 const CheckoutForm = ({ bookingId }) => {
   console.log(bookingId);
+  const { getToken } = useAuth();
 
-  const fetchClientSecret = useCallback(() => {
+  const fetchClientSecret = useCallback(async () => {
+    const token = await getToken();
     // Create a Checkout Session
-    return fetch(`${BACKEND_URL}/api/payments/create-checkout-session`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ bookingId }),
-    })
-      .then((res) => res.json())
-      .then((data) => data.clientSecret);
+    const res = await fetch(
+      `${BACKEND_URL}/api/payments/create-checkout-session`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+          body: JSON.stringify({ bookingId }),
+        },
+      }
+    );
+    const data = await res.json();
+    return data.clientSecret;
   }, []);
 
   const options = { fetchClientSecret };
